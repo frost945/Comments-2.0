978d67119d25d3a34483ee941a8d8f350d6208c6
frost <setter845jd@gmail.com>
2026-04-22 01:07:32 +0300

    fix: fix HTML validation by adding normalization for accurate comparison of input and sanitized HTML

--- Frontend/src/app/shared/utils/html-sanitizer.util.ts
+++ Frontend/src/app/shared/utils/html-sanitizer.util.ts
@@ -1,41 +1,53 @@
-
 const SANITIZER_CONFIG = {
   ALLOWED_TAGS: ['a', 'code', 'i', 'strong'],
   ALLOWED_ATTR: ['href', 'title'],
   ALLOW_DATA_ATTR: false
 };
 
- export function sanitizeHtml(input: string): string {
-
-  if (typeof DOMPurify === 'undefined') {
-        console.warn('DOMPurify is unavailable');
-        return input;
-    }
-
+// clean up HTML and adding necessary attributes to links
+export function sanitizeHtml(input: string): string {
   if (!input) return '';
 
-   let cleaned = DOMPurify.sanitize(input, SANITIZER_CONFIG);
+  const hook = (node: Element) => {
+    if (node.tagName === 'A') {
+      node.setAttribute('target', '_blank');
+      node.setAttribute('rel', 'noopener noreferrer');
+    }
+  };
 
-  // добавляем target="_blank" и rel="noopener noreferrer" ко всем ссылкам
-  cleaned = cleaned.replace(
-    /<a\s+([^>]+)>/g,
-    '<a $1 target="_blank" rel="noopener noreferrer">'
-  );
+  DOMPurify.addHook('afterSanitizeAttributes', hook);
 
+  const cleaned = DOMPurify.sanitize(input, SANITIZER_CONFIG);
+
+  DOMPurify.removeHook('afterSanitizeAttributes', hook);
+
+  console.debug('Sanitized HTML cleaned:', cleaned);
   return cleaned;
 }
 
-export function validateHtml(input: string) {
+// remove attributes to ensure correct comparison of original and cleaned HTML
+function normalizeForCompare(html: string): string {
+  return html
+    .replace(/\s+/g, ' ')
+    .replace(/target="_blank"/g, '')
+    .replace(/rel="noopener noreferrer"/g, '')
+    .trim();
+}
 
+export function validateHtml(input: string) {
   if (!input) {
     return { isValid: true, cleaned: '' };
   }
+  // use basic sanitizer, otherwise the comparison will be incorrect due to the attributes added to the links
+  const cleaned = DOMPurify.sanitize(input, SANITIZER_CONFIG);
 
-  const cleaned = sanitizeHtml(input);
-  const isValid = cleaned === input;
+  const normalizedInput = normalizeForCompare(input);
+  const normalizedCleaned = normalizeForCompare(cleaned);
 
-  return {
-    isValid,
-    cleaned
-  };
+  console.debug('normalizedInput:', normalizedInput);
+  console.debug('normalizedCleaned:', normalizedCleaned);
+
+  const isValid = normalizedInput === normalizedCleaned;
+
+  return { isValid, cleaned};
 }
